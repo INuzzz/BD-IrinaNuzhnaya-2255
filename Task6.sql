3906de63b8ab27826a5e2336b63bee4ec04d8a1f
Irina <nia.19@uni-dubna.ru>
2021-04-20 19:15:50 +0300

    Исправление заданий 1-5

--- Task6.sql
+++ Task6.sql
@@ -1,10 +1,11 @@
-with new_table as (select count(sh.id_hobby) as count_hobby
+with new_table as (select sh.id_student, count(sh.id_hobby) as count_hobby
 	  from student_hobby sh
 	  group by sh.id_student)
 
 select avg(s.score)::real, s.n_group
-from new_table nt, student s
+from student s
 inner join student_hobby sh on s.id = sh.id_student
+inner join new_table nt on sh.id_student = nt.id_student
 where nt.count_hobby >=1
 group by s.n_group
 order by s.n_group
